846572dc7af9f3324c4106a918462cb9a90e9717
Patrick Murphy <murph.pm@gmail.com>
2014-06-07 00:02:30 -0500

    Added some documentation that was missing from the previous commit

---
+++ OccludedLibrary/docs/html/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['prepare_5ffor_5frender',['prepare_for_render',['../classoccluded_1_1opengl_1_1retained_1_1gl__attribute__buffer.html#ab7707122e284ee1c28bf238885ae6a59',1,'occluded::opengl::retained::gl_attribute_buffer']]]
+];
